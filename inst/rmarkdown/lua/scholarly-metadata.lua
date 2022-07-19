b4a7116dacea394ba1b3d79a28b3e1c4d67685ea
Julia Schulte-Cloos <julia.schulte@lmu.de>
2022-07-19 15:30:06 +0200

    update lua filters to fix issue #1

--- inst/rmarkdown/lua/scholarly-metadata.lua
+++ inst/rmarkdown/lua/scholarly-metadata.lua
@@ -1,12 +1,9 @@
 --[[
 ScholarlyMeta – normalize author/affiliation meta variables
-
 Copyright (c) 2017-2021 Albert Krewinkel, Robert Winkler
-
 Permission to use, copy, modify, and/or distribute this software for any purpose
 with or without fee is hereby granted, provided that the above copyright notice
 and this permission notice appear in all copies.
-
 THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
 REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
 FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
@@ -16,6 +13,21 @@ TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
 THIS SOFTWARE.
 ]]
 local List = require 'pandoc.List'
+
+--- Returns the type of a metadata value.
+--
+-- @param v a metadata value
+-- @treturn string one of `Blocks`, `Inlines`, `List`, `Map`, `string`, `boolean`
+local function metatype (v)
+  if PANDOC_VERSION <= '2.16.2' then
+    local metatag = type(v) == 'table' and v.t and v.t:gsub('^Meta', '')
+    return metatag and metatag ~= 'Map' and metatag or type(v)
+  end
+  return pandoc.utils.type(v)
+end
+
+local type = pandoc.utils.type or metatype
+
 
 -- Split a string at commas.
 local function comma_separated_values(str)
@@ -28,20 +40,19 @@ end
 
 --- Ensure the return value is a list.
 local function ensure_list (val)
-  if type(val) ~= 'table' then
-    -- create singleton list (or empty list if val == nil).
-    return List:new{val}
-  elseif val.t == 'MetaInlines' then
+  if type(val) == 'List' then
+    return val
+  elseif type(val) == 'Inlines' then
     -- check if this is really a comma-separated list
     local csv = comma_separated_values(pandoc.utils.stringify(val))
     if #csv >= 2 then
       return csv
     end
     return List:new{val}
-  elseif val.t == 'MetaList' then
+  elseif type(val) == 'table' and #val > 0 then
     return List:new(val)
   else
-    -- MetaBlocks or MetaMap, use as a singleton
+    -- Anything else, use as a singleton (or empty list if val == nil).
     return List:new{val}
   end
 end
@@ -67,23 +78,23 @@ end
 -- (`nil`).
 function to_named_object (obj)
   local named = {}
-  if type(obj) ~= 'table' then
+  if type(obj) == 'Inlines' then
+    -- Treat inlines as the name
+    named.name = obj
+    named.id = pandoc.utils.stringify(obj)
+  elseif type(obj) ~= 'table' then
     -- if the object isn't a table, just use its value as a name.
     named.name = pandoc.MetaInlines{pandoc.Str(tostring(obj))}
     named.id = tostring(obj)
-  elseif obj.t == 'MetaInlines' then
-      -- Treat inlines as the name
-      named.name = obj
-      named.id = pandoc.utils.stringify(obj)
   elseif obj.name ~= nil then
     -- object has name attribute → just create a copy of the object
     add_missing_entries(obj, named)
     named.id = pandoc.utils.stringify(named.id or named.name)
   elseif next(obj) and next(obj, next(obj)) == nil then
-    -- the entry's key is taken as the name, the value contains the
-    -- attributes.
+    -- Single-entry table. The entry's key is taken as the name, the value
+    -- contains the attributes.
     key, attribs = next(obj)
-    if type(attribs) == "string" or attribs.t == 'MetaInlines' then
+    if type(attribs) == 'string' or type(attribs) == 'Inlines' then
       named.name = attribs
     else
       add_missing_entries(attribs, named)
